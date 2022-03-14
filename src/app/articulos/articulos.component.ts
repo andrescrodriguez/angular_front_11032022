6fed2ab9739e2068bdcb03708aa71f92b8b5c011
Andrés <andres.c.rodriguez@outlook.com>
2022-03-14 18:17:05 -0300

    paginador - antes de agregar el metodo para navegar usando los botones de las paginas

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -1,3 +1,4 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Articulo } from './articulos';
@@ -11,23 +12,54 @@ import { ArticulosService } from './articulos.service';
 })
 export class ArticulosComponent implements OnInit {
 
+  
+  paginaActual: number = 1;
+  cantidadDePaginas: number = 1;
+  cantidadDePaginasArr: number[];
+  cantidadDeRegistrosAMostrar: number = 3;  
+  articulos: Articulo[];
+  cantidadTotalDeRegistros: any;
+
   constructor(private articulosService: ArticulosService, private router: Router) { }
   
-  articulos: Articulo[] = [];
-
   ngOnInit(): void {
-    this.getAll();
-    console.log("resul: ");
+    this.getAll(this.paginaActual, this.cantidadDeRegistrosAMostrar);
   }
 
-  getAll(){
-    this.articulosService.getAll(1, 10).subscribe(n => {
-      console.log("resultado: ");
-      console.log(n);
-    },
-    e => {
+  getAll(pagina: number, cantidadDeRegistrosAMostrar: number) {
+    this.articulosService.getAll(pagina, cantidadDeRegistrosAMostrar).subscribe((result : HttpResponse<Articulo[]>) => {
+     this.cantidadTotalDeRegistros = result.headers.get('cantidadTotalDeRegistros');
+     this.cantidadDePaginas = Math.ceil(this.cantidadTotalDeRegistros / this.cantidadDeRegistrosAMostrar);
+     this.articulos = result.body!;
 
-    })
+     // inicio - manejo de los botones del paginador
+     this.cantidadDePaginasArr = [];
+     if(this.paginaActual > 1){
+      this.cantidadDePaginasArr.push(this.paginaActual - 1);
+     }
+     for (var i = this.paginaActual; i <= (this.paginaActual + 1); i++){
+      this.cantidadDePaginasArr.push(i);
+     }
+     if(this.cantidadDePaginasArr[this.cantidadDePaginasArr.length -1] > this.cantidadDePaginas){
+      this.cantidadDePaginasArr.pop();
+     }
+     // fin - manejo de los botones del paginador
+     
+    });
+  }
+
+  anterior() {
+    if(this.paginaActual > 1){
+      this.paginaActual -= 1;
+      this.getAll(this.paginaActual, this.cantidadDeRegistrosAMostrar);
+    }
+  }
+
+  siguiente() {
+    if (this.cantidadDePaginas > this.paginaActual){
+      this.paginaActual += 1;
+      this.getAll(this.paginaActual, this.cantidadDeRegistrosAMostrar);
+    }
   }
 
   redireccionar(id: number) {
